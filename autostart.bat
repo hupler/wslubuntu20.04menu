abb2f77e3db4bc3e96900e80a23b8bf820525d54
hupler <150682593+hupler@users.noreply.github.com>
2024-01-14 08:13:45 +0700

    Update autostart.bat

--- autostart.bat
+++ autostart.bat
@@ -1,36 +1,4 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAnk
-::fBw5plQjdG8=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSzk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCyDJGyX8VAjFCJVXwmLMleeCaIS5Of66/m7t08IWvYqOJjI1vSDJe4G1kTxYZk+wntIjIUJFB44
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
+set /p UNAME=< uname.txtd
 echo Starting Ubuntu-20.04...
 wsl -t Ubuntu-20.04
 wsl -d Ubuntu-20.04 -u %UNAME% bash /home/%UNAME%/start.sh
